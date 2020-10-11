90fe1655240f9bc51872b41a20304982ce424007
gitlab_admin <admin@f-lab.tech>
2020-10-11 15:31:39 +0000

    Task 03 [Плешков]

--- task03/src/com/example/task03/Minutes.java
+++ task03/src/com/example/task03/Minutes.java
@@ -1,27 +1,32 @@
 package com.example.task03;
 
 public class Minutes implements TimeUnit {
+    private final long amount;
 
     public Minutes(long amount) {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        this.amount = amount;
     }
 
     @Override
     public long toMillis() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+        return this.amount * 60 * 1000;
     }
 
     @Override
     public long toSeconds() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+        return this.amount * 60;
     }
 
     @Override
     public long toMinutes() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException();
+        return this.amount;
+    }
+
+    @Override
+    public long toHours() {
+        return Math.round(this.amount / 60d);
     }
 }
