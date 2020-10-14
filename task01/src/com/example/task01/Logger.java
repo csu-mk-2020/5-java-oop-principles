ed0fb2a2fce3cd07c36c02d58bf39cd54b9064b1
Arthur <parabellum357@hotmail.com>
2020-10-14 11:10:18 +0500

    Task 01 [Саитгалин]

--- task01/src/com/example/task01/Logger.java
+++ task01/src/com/example/task01/Logger.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 public class Logger {
     private static final Hashtable<String, Logger> loggers = new Hashtable<>();
-    private final String name;
+    protected final String name;
     private LoggerLevel level = LoggerLevel.ERROR;
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat(" yyyy.MM.dd hh:mm:ss ");
 
@@ -31,49 +31,49 @@ public class Logger {
         return level;
     }
 
-    void log(LoggerLevel level, String message) {
+    private void log(LoggerLevel level, String message) {
         if (level.ordinal() >= this.level.ordinal()) {
             System.err.println("[" + this.level.toString() + "]" + dateFormat.format(new Date()) +
                     this.name + " - " + message);
         }
     }
 
-    void log(LoggerLevel level, String format, Object... objects) {
+    private void log(LoggerLevel level, String format, Object... objects) {
         if (level.ordinal() >= this.level.ordinal()) {
             System.err.println("[" + this.level.toString() + "]" + dateFormat.format(new Date()) +
                     this.name + " - " + String.format(format, objects));
         }
     }
 
-    void debug(String message) {
+    public void debug(String message) {
         this.log(LoggerLevel.DEBUG, message);
     }
 
-    void debug(String format, Object... objects) {
+    public void debug(String format, Object... objects) {
         this.log(LoggerLevel.DEBUG, format, objects);
     }
 
-    void info(String message) {
+    public void info(String message) {
         this.log(LoggerLevel.INFO, message);
     }
 
-    void info(String format, Object... objects) {
+    public void info(String format, Object... objects) {
         this.log(LoggerLevel.INFO, format, objects);
     }
 
-    void warning(String message) {
+    public void warning(String message) {
         this.log(LoggerLevel.WARNING, message);
     }
 
-    void warning(String format, Object... objects) {
+    public void warning(String format, Object... objects) {
         this.log(LoggerLevel.WARNING, format, objects);
     }
 
-    void error(String message) {
+    public void error(String message) {
         this.log(LoggerLevel.ERROR, message);
     }
 
-    void error(String format, Object... objects) {
+    public void error(String format, Object... objects) {
         this.log(LoggerLevel.ERROR, format, objects);
     }
 
