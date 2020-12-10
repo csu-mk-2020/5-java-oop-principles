14f5d4c338dbc4fd7364bc5e4694c65561adaa6d
Aleksey <alex-kadykov@bk.ru>
2020-12-10 16:33:18 +0500

    task01 [Кадыков]

---
+++ task01/src/com/example/task01/LoggerLevel.java
@@ -0,0 +1,8 @@
+package com.example.task01;
+
+public enum LoggerLevel {
+    DEBUG,      // - отладка
+    INFO,       // - информационные сообщения
+    WARNING,    // - предупреждение
+    ERROR       // - ошибка
+}
