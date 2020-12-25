c586dba046c305112621cca63d209da1e2eb1e16
FinnLEA <56691691+FinnLEA@users.noreply.github.com>
2020-12-25 16:00:41 +0500

    Веденькин task01

---
+++ task01/src/com/example/task01/Logger.java
@@ -0,0 +1,146 @@
+package com.example.task01;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+
+enum LogLevels {
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR
+}
+
+public class Logger {
+    private LogLevels thisLevel;
+    private String name;
+    private static int maxLevel = 0;
+    private static List<Logger> loggerList = new ArrayList<>();
+
+    public Logger(String Name) {
+        name = Name;
+        loggerList.add(this);
+    }
+
+    public static Logger getLogger(String Name) {
+        for (Logger e : loggerList) {
+            if (e.name.equals(Name)) {
+                return e;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public LogLevels getLevel() {
+        return thisLevel;
+    }
+
+    public void setLevel(LogLevels lvl) {
+        thisLevel = lvl;
+
+        switch (lvl) {
+            case DEBUG:
+                maxLevel = 1;
+                break;
+            case INFO:
+                maxLevel = 2;
+                break;
+            case ERROR:
+                maxLevel = 3;
+                break;
+            case WARNING:
+                maxLevel = 4;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void log(LogLevels level, String message) {
+        log(level, message, (Object) null);
+    }
+
+    public void log(LogLevels level, String message, Object... obj) {
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        System.out.println("[" + level + "] " + sdfDate.format(new Date()) + " " + this.name + " - " + String.format(message, obj));
+    }
+
+    public void debug(String msg) {
+        if (maxLevel < 1) {
+            setLevel(LogLevels.DEBUG);
+        }
+        if (maxLevel == 1) {
+            log(LogLevels.DEBUG, msg);
+        }
+    }
+
+    public void debug(String template, Object... obj) {
+        if (maxLevel < 1) {
+            setLevel(LogLevels.DEBUG);
+        }
+        if (maxLevel == 1) {
+            log(LogLevels.DEBUG, template, obj);
+        }
+    }
+
+    public void info(String msg) {
+        if (maxLevel < 2) {
+            setLevel(LogLevels.INFO);
+        }
+        if (maxLevel == 2) {
+            log(LogLevels.INFO, msg);
+        }
+    }
+
+    public void info(String template, Object... obj) {
+        if (maxLevel < 2) {
+            setLevel(LogLevels.INFO);
+        }
+        if (maxLevel == 2) {
+            log(LogLevels.INFO, template, obj);
+        }
+    }
+
+    public void warning(String msg) {
+        if (maxLevel < 3) {
+            setLevel(LogLevels.WARNING);
+        }
+        if (maxLevel == 3) {
+            log(LogLevels.WARNING, msg);
+        }
+    }
+
+    public void warning(String template, Object... obj) {
+        if (maxLevel < 3) {
+            setLevel(LogLevels.WARNING);
+        }
+        if (maxLevel == 3) {
+            log(LogLevels.WARNING, template, obj);
+        }
+    }
+
+    public void error(String msg) {
+        if (maxLevel < 4) {
+            setLevel(LogLevels.ERROR);
+        }
+        if (maxLevel == 4) {
+            log(LogLevels.ERROR, msg);
+        }
+    }
+
+    public void error(String template, Object... obj) {
+        if (maxLevel < 4) {
+            setLevel(LogLevels.ERROR);
+        }
+        if (maxLevel == 4) {
+            log(LogLevels.ERROR, template, obj);
+        }
+    }
+}
